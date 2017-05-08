f2cc68b0f8a84843b4a0ba0fe33d27a67528a158
valeriyager <gerasimenkovaleriya.i@gmail.com>
2017-05-08 17:45:19 +0300

    add function for evaluating a new direction for the photon (based on a_factor)

--- src/transport.cu
+++ src/transport.cu
@@ -32,3 +32,39 @@ __global__ void LaunchPhoton_Global(MemStruct DeviceMem) {
 
   DeviceMem.p[init + tx] = p; 
 }
+
+__device__ void Spin(PhotonStruct * p, float g, unsigned long long * x, unsigned int * a) {
+  float cost, sint; // cosine and sine of the polar deflection angle theta
+  float cosp, sinp; // cosine and sine of the azimuthal angle psi. 
+  float temp;
+
+  float tempdir = p -> dx;
+
+  temp = __fdividef((1.0f - (g) * (g)), (1.0f - (g) + 2.0f * (g) * rand_MWC_co(x, a)));
+  cost = __fdividef((1.0f + (g) * (g) - temp * temp), (2.0f * (g)));
+  if (g == 0.0f)
+    cost = 2.0f * rand_MWC_co(x, a) - 1.0f;
+
+  sint = sqrtf(1.0f - cost * cost);
+
+  __sincosf(2.0f * PI * rand_MWC_co(x, a), & sinp, & cosp);
+
+  temp = sqrtf(1.0f - p -> dz * p -> dz);
+
+  if (temp == 0.0f) {
+    //normal incident.
+    p -> dx = sint * cosp;
+    p -> dy = sint * sinp;
+    p -> dz = copysignf(cost, p -> dz * cost);
+  } else {
+    // regular incident.
+    p -> dx = __fdividef(sint * (p -> dx * p -> dz * cosp - p -> dy * sinp), temp) + p -> dx * cost;
+    p -> dy = __fdividef(sint * (p -> dy * p -> dz * cosp + tempdir * sinp), temp) + p -> dy * cost;
+    p -> dz = -sint * cosp * temp + p -> dz * cost;
+  }
+
+  temp = rsqrtf(p -> dx * p -> dx + p -> dy * p -> dy + p -> dz * p -> dz);
+  p -> dx = p -> dx * temp;
+  p -> dy = p -> dy * temp;
+  p -> dz = p -> dz * temp;
+}
