04482d10f739553decb1630092617ee6924939c3
valeriyager <gerasimenkovaleriya.i@gmail.com>
2017-05-08 17:39:06 +0300

    add function for setting the initial state of a photon

--- src/transport.cu
+++ src/transport.cu
@@ -7,3 +7,14 @@ __device__ void Spin(PhotonStruct * , float, unsigned long long * , unsigned int
 __device__ unsigned int Reflect(PhotonStruct * , int, unsigned long long * , unsigned int * );
 __device__ unsigned int PhotonSurvive(PhotonStruct * , unsigned long long * , unsigned int * );
 __device__ void AtomicAddULL(unsigned long long * address, unsigned int add);
+
+__device__ void LaunchPhoton(PhotonStruct * p, unsigned long long * x, unsigned int * a) {
+  p -> x = 0.0f;
+  p -> y = 0.0f;
+  p -> z = 0.0f;
+  p -> dx = 0.0f;
+  p -> dy = 0.0f;
+  p -> dz = 1.0f;
+  p -> layer = 1;
+  p -> weight = * start_weight_dc;
+}
